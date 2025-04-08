837c0540181249b4a625b575bd9248144e7c7a93
Henry Velesaca Lara <51929556+hvelesaca@users.noreply.github.com>
2025-04-08 15:52:14 -0500

    Update dataloader.py

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -8,42 +8,57 @@ import torchvision.transforms as transforms
 import numpy as np
 import torch
 
+import os
+from PIL import Image
+import torch.utils.data as data
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+import numpy as np
+import torch
+
 class CODataset(data.Dataset):
-    def __init__(self, image_root, gt_root, trainsize, augmentations):
+    def __init__(self, image_root, gt_root, trainsize, augmentations=True):
         self.trainsize = trainsize
         self.augmentations = augmentations
-        print(f"Augmentations: {self.augmentations}")
 
-        self.images = sorted([os.path.join(image_root, f) for f in os.listdir(image_root) if f.lower().endswith(('.jpg', '.png', '.jpeg'))])
-        self.gts = sorted([os.path.join(gt_root, f) for f in os.listdir(gt_root) if f.lower().endswith(('.jpg', '.png', '.jpeg'))])
+        self.images = sorted([
+            os.path.join(image_root, f) for f in os.listdir(image_root)
+            if f.lower().endswith(('.jpg', '.png', '.jpeg'))
+        ])
+        self.gts = sorted([
+            os.path.join(gt_root, f) for f in os.listdir(gt_root)
+            if f.lower().endswith(('.jpg', '.png', '.jpeg'))
+        ])
 
         self.filter_files()
         self.size = len(self.images)
 
+        self.transform = self.get_transforms()
+
+    def get_transforms(self):
         if self.augmentations:
-            print('Using Albumentations for augmentation')
-            self.transform = A.Compose([
-                A.RandomRotate90(p=0.5),
-                A.VerticalFlip(p=0.5),
-                A.HorizontalFlip(p=0.5),
+            print('Using advanced Albumentations for augmentation')
+            return A.Compose([
                 A.Resize(self.trainsize, self.trainsize),
+                A.HorizontalFlip(p=0.5),
+                A.VerticalFlip(p=0.5),
+                A.RandomRotate90(p=0.5),
+                A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.1, rotate_limit=45, p=0.5, border_mode=0),
+                A.RandomBrightnessContrast(p=0.5),
+                A.GaussNoise(p=0.3),
+                A.HueSaturationValue(p=0.3),
                 A.Normalize(mean=(0.485, 0.456, 0.406),
                             std=(0.229, 0.224, 0.225)),
                 ToTensorV2()
             ])
         else:
             print('No augmentation, only resizing and normalization')
-            self.transform = A.Compose([
+            return A.Compose([
                 A.Resize(self.trainsize, self.trainsize),
                 A.Normalize(mean=(0.485, 0.456, 0.406),
                             std=(0.229, 0.224, 0.225)),
                 ToTensorV2()
             ])
-
-        self.gt_transform = A.Compose([
-            A.Resize(self.trainsize, self.trainsize),
-            ToTensorV2()
-        ])
 
     def __getitem__(self, index):
         image = np.array(Image.open(self.images[index]).convert('RGB'))
@@ -56,7 +71,7 @@ class CODataset(data.Dataset):
         return image, gt
 
     def filter_files(self):
-        assert len(self.images) == len(self.gts)
+        assert len(self.images) == len(self.gts), "Mismatch between images and ground truths"
         images, gts = [], []
         for img_path, gt_path in zip(self.images, self.gts):
             img = Image.open(img_path)
@@ -69,7 +84,7 @@ class CODataset(data.Dataset):
     def __len__(self):
         return self.size
 
-def get_loader(image_root, gt_root, batchsize, trainsize, shuffle=True, num_workers=4, pin_memory=True, augmentation=False):
+def get_loader(image_root, gt_root, batchsize, trainsize, shuffle=True, num_workers=4, pin_memory=True, augmentation=True):
     dataset = CODataset(image_root, gt_root, trainsize, augmentation)
     data_loader = data.DataLoader(dataset=dataset,
                                   batch_size=batchsize,
