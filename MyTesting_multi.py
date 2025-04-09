d447abbdd966812779e79338fbe0dfa81fc4230b
Henry Velesaca Lara <51929556+hvelesaca@users.noreply.github.com>
2025-04-09 16:16:49 -0500

    Delete MyTesting_multi.py

--- MyTesting_multi.py
+++
@@ -1,9 +0,0 @@
-import torch
-from lib.mamba_unet import VisionMambaPVTUNet
-
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-model = VisionMambaPVTUNet(pretrained=False).to(device)
-model.eval()
-
-dummy_input = torch.randn(1, 3, 256, 256).to(device)
-torch.onnx.export(model, dummy_input, "vision_mamba.onnx", opset_version=12)
