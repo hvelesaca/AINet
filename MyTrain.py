bd0cc70db63c691cb4812a9aa39463cc5605866a
Henry Velesaca Lara <51929556+hvelesaca@users.noreply.github.com>
2025-04-19 16:17:20 -0500

    Update MyTrain.py

--- MyTrain.py
+++ MyTrain.py
@@ -234,7 +234,7 @@ if __name__ == '__main__':
     parser.add_argument('--trainsize', type=int,default=352, help='training dataset size,candidate=352,704,1056')
     parser.add_argument('--clip', type=float,default=0.5, help='gradient clipping margin')
     parser.add_argument('--load', type=str, default=None, help='train from checkpoints')
-    parser.add_argument('--decay_rate', type=float,default=0.1, help='decay rate of learning rate')
+    parser.add_argument('--decay_rate', type=float,default=0.01, help='decay rate of learning rate')
     parser.add_argument('--decay_epoch', type=int,default=20, help='every n epochs decay learning rate')
     parser.add_argument('--train_path', type=str,default=f'{dataset}/train',help='path to train dataset')
     parser.add_argument('--test_path', type=str,default=f'{dataset}/val',help='path to testing dataset')
