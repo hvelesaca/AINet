4a3b0eab7e222e2b31a58ac03c87e03035856fa0
Henry Velesaca Lara <51929556+hvelesaca@users.noreply.github.com>
2025-04-28 17:04:44 -0500

    Update MyTrain.py

--- MyTrain.py
+++ MyTrain.py
@@ -125,16 +125,52 @@ def dice_loss(predict, target):
     return loss.mean()
     
 def structure_loss(pred, mask):
+    # Weighted BCE
     weit = 1 + 5 * torch.abs(F.avg_pool2d(mask, kernel_size=31, stride=1, padding=15) - mask)
     wbce = F.binary_cross_entropy_with_logits(pred, mask, reduction='none')
     wbce = (weit * wbce).sum(dim=(2, 3)) / weit.sum(dim=(2, 3))
 
+    # Weighted IoU
     pred = torch.sigmoid(pred)
     inter = ((pred * mask) * weit).sum(dim=(2, 3))
     union = ((pred + mask) * weit).sum(dim=(2, 3))
     wiou = 1 - (inter + 1) / (union - inter + 1)
 
     return (wbce + wiou).mean()
+
+
+def hybrid_e_loss(pred, mask):
+    """
+        SciChina-Hybrid Eloss-2020
+    :param pred:
+    :param mask:
+    :return:
+    """
+    # adaptive weighting masks
+    weit = 1 + 5 * torch.abs(F.avg_pool2d(mask, kernel_size=31, stride=1, padding=15) - mask)
+
+    # weighted binary cross entropy loss function
+    wbce = F.binary_cross_entropy_with_logits(pred, mask, reduce='none')
+    wbce = ((weit * wbce).sum(dim=(2, 3)) + 1e-8) / (weit.sum(dim=(2, 3)) + 1e-8)
+
+    # weighted e loss function
+    pred = torch.sigmoid(pred)
+    mpred = pred.mean(dim=(2, 3)).view(pred.shape[0], pred.shape[1], 1, 1).repeat(1, 1, pred.shape[2], pred.shape[3])
+    phiFM = pred - mpred
+
+    mmask = mask.mean(dim=(2, 3)).view(mask.shape[0], mask.shape[1], 1, 1).repeat(1, 1, mask.shape[2], mask.shape[3])
+    phiGT = mask - mmask
+
+    EFM = (2.0 * phiFM * phiGT + 1e-8) / (phiFM * phiFM + phiGT * phiGT + 1e-8)
+    QFM = (1 + EFM) * (1 + EFM) / 4.0
+    eloss = 1.0 - QFM.mean(dim=(2, 3))
+
+    # weighted iou loss function
+    inter = ((pred * mask) * weit).sum(dim=(2, 3))
+    union = ((pred + mask) * weit).sum(dim=(2, 3))
+    wiou = 1.0 - (inter + 1 + 1e-8) / (union - inter + 1 + 1e-8)
+
+    return (wbce + eloss + wiou).mean()
 
 def val(model, epoch, save_path, writer):
     """
@@ -213,14 +249,14 @@ def train(train_loader, model, optimizer, epoch, test_path):
             #Edge loss function
             loss_Edge = dice_loss(P_edge, edge)
             # ---- loss function ----
-            losses = [structure_loss(out, gts) for out in P1]
+            losses = [hybrid_e_loss(out, gts) for out in P1]
             loss_P1 = 0
             gamma = 0.25
             #print('iteration num',len(P1))
             for it in range(len(P1)):
                 loss_P1 += (gamma * (it)) * losses[it]
 
-            loss_P2 = structure_loss(P2, gts)
+            loss_P2 = hybrid_e_loss(P2, gts)
 
             loss = loss_P1 + loss_P2
 
