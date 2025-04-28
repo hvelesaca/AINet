64826f3950263175d773a35d633313de4309efd0
Henry Velesaca Lara <51929556+hvelesaca@users.noreply.github.com>
2025-04-28 15:58:35 -0500

    Update MyTesting.py

--- MyTesting.py
+++ MyTesting.py
@@ -72,7 +72,7 @@ for _data_name in [opt.test_path]:
         res = res.sigmoid().data.cpu().numpy().squeeze()
         res = (res - res.min()) / (res.max() - res.min() + 1e-8)
         print('> {} - {}'.format(_data_name, name))
-        cv2.imwrite(save_path+"/out3out2final/"+name,res*255)
+        cv2.imwrite(save_path+"/out3out2out1final/"+name,res*255)
 
         #os.makedirs(save_path+"/out1", exist_ok=True)
         #res = F.upsample(P1[0], size=gt.shape, mode='bilinear', align_corners=False)
